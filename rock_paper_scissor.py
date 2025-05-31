639c5fb0f3cab0cb0455ffe8e7a72b8e19ef055a
codewith-Aisha <subhasreemantry@gmail.com>
2025-05-31 11:51:55 +0530

    Add files via upload

---
+++ rock_paper_scissor.py
@@ -0,0 +1,54 @@
+# #lipun_soni
+# import random 
+# rock = '''
+#    ------(""""")
+#    -----------(""""")
+#   ------------(""""")
+#   -----------(""""")
+#   -----------("""")
+# '''
+# paper = '''
+#   -----(""""")
+#  ----------------(""""")
+# --------------------(""""")
+#  ------------------(""""")
+#   -------------(""""")
+# '''
+
+# scissor = '''
+#   -----(""""")
+#  --------------------(""""")
+# -------------------------(""""")
+# ---------(""""")
+# --------(""""")
+# '''
+# wrong = '''
+# ...      ...
+#  ...    ...
+#    ... ... 
+#     ....  
+#   ...  ...   
+#  ...    ...   
+# ...      ...
+# '''
+# game = [rock,paper,scissor]
+# user_choice =int( input ("wellcome to rock paper scissor\n\nFor rock enter 0\nFor paper enter 1\nFor scissor enter 2\nSo. what is your choice :"))
+
+# if user_choice > 2 :
+#     print (f"wrong input\n{wrong}")
+# else:
+#     computer_choice = random.randint(0,2)
+#     print(game[user_choice])
+#     print ("computer choice")
+#     print (game[computer_choice])
+
+#     if user_choice == computer_choice:
+#         print("draw")
+#     elif user_choice == 0 and computer_choice == 2:
+#         print("you are the winner")
+#     elif user_choice == 2 and computer_choice == 0:
+#         print("you are the losser")
+#     elif user_choice > computer_choice:
+#         print("you are the winner")
+#     elif user_choice < computer_choice:
+#         print("you are the losser")
